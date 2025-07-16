d245dcd2ca30da7acc51119d8991b50681b87cf2
thiagooo14 <thiagooo15@gmail.com>
2025-07-15 21:19:27 -0300

    chore: adiciona configuração do Vitest e atualiza dependências no projeto

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
   base: '/thiagooo14-Portifoil',
 });
